5867da87a570c5b330cacb3e8613ba11ea7c8356
ajamol2197 <ajamol2197@gmail.com>
2025-11-13 18:01:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+# 1-m
+a = '    phyton dasturlash     '
+print(len(a))
+print(a.lstrip())
+
+# 2-m
+a  = 'hello world'
+print(a.upper())
+print(len(a))
+
+# 3-m
+a = 'SALOM DUNYO 123'
+print(a.lower())
+print(a.strip())
+
+# 4-m
+a = "     ozbekiston     "
+print(a.rstrip())
+print(a.capitalize())
+
+# 5-m
+a = "phyton3.11"
+print(a.title())
+print(len(a*2))
+
+# 6-m
+a = "    toshkent 2025   "
+print(a.strip())
+print(a.upper())
+
+# 7-m
+a = "ALGORITM VA DASTURLASH"
+print(a.lower())
+print(a.lstrip())
+print(len(a))
+
+# 8-m
+a=  " java vs phyton  "
+print(a.title())
+print(a.rstrip())
+
+# 9-m
+a = "oquvchi talaba"
+print(a.capitalize())
+print(len(a))
+
+ # 10-m
+a = '    DATA SCINSE     '
+print(a.strip())
+print(a.upper())
+print(a.lower)
+
+
+
+
+
+
+
+
